841b3c1ac794d1f871ac06e543759baba00e1b70
tnightingale <tom@tnightingale.com>
2013-04-12 14:30:58 -0700

    improved dump save api

--- test/dump_test.js
+++ test/dump_test.js
@@ -26,4 +26,21 @@ exports['dump'] = {
 
         test.done();
     },
+
+    'save': function (test) {
+        test.expect(4);
+        var dump = new Dump(),
+            when_saved = dump.save({ foo: 'bar' });
+
+        test.ok('then' in when_saved && typeof when_saved.then === 'function', "save() should return a Promises/A promise.");
+
+        when_saved.then(function (params) {
+            test.ok(params.length === 2, "save() should return two parameters; [id, data].");
+            test.ok(params[0] !== null, "param[0] should be a valid id.");
+            test.deepEqual(params[1], { foo: 'bar' }, 'param[1] should match saved data.');
+            test.done();
+        }, function () {
+            test.ok(false, "save({ foo: 'bar' } shouldn't fail."); test.done();
+        });
+    }
 };
